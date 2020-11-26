dee002eccc27c2cf03c909b01227d4df72a8233a
bigwizzo <wnnwillard@gmail.com>
2020-11-26 23:46:19 +0200

    Add HTML form in the new.html.erb and the Create method in the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,37 @@
 class UsersController < ApplicationController
 
   def new 
- 
+   @user = User.new
   end
+  
+
+  # def create
+    # @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+    # if @user.save
+      # flash[:success] = "User successfully created"
+      # redirect_to new_user_path
+    # else
+      # flash[:error] = "Something went wrong"
+      # render 'new'
+    # end
+  # end
 
   def create
-
+    # @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+    @user = User.new(whitelist)
+  
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def whitelist
+    params.require(:user).permit(:user_name, :email, :password)
+  end
+  
 
 end
