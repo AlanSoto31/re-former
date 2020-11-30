ad6a73b921bd71253b114fa0a14e15b12574a89b
alansoto31 <soto.alan@hotmail.com>
2020-11-30 14:51:41 -0600

    Add editing section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,47 @@
 class UsersController < ApplicationController
-
+  include UsersHelper
   def new 
    @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     #@user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
-    @user = User.new(whitelist)
+    @user = User.new(user_params)
   
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
     end
   end
 
-  private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-  def whitelist
-    params.require(:user).permit(:user_name, :email, :password)
+  #def update
+  #  @user = User.find(params[:id])
+  #  @user.update(user_params)
+  #  redirect_to edit_user_path
+  #end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Object was successfully updated"
+        redirect_to edit_user_path
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
   end
   
-
 end
