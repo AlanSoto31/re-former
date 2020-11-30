4a95c7d6bd480b00fa715d857f4586568c0b88b3
bigwizzo <wnnwillard@gmail.com>
2020-11-30 23:27:57 +0200

    Fix linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   include UsersHelper
-  def new 
-   @user = User.new
+  def new
+    @user = User.new
   end
 
   def index
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+    # @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-  
+
     if @user.save
       redirect_to new_user_path
     else
@@ -27,21 +27,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  #def update
-  #  @user = User.find(params[:id])
-  #  @user.update(user_params)
-  #  redirect_to edit_user_path
-  #end
-
   def update
     @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:success] = "Object was successfully updated"
-        redirect_to edit_user_path
-      else
-        flash[:error] = "Something went wrong"
-        render 'edit'
-      end
+    if @user.update(user_params)
+      flash[:success] = 'Object was successfully updated'
+      redirect_to edit_user_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'edit'
+    end
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #resource :users, only: [:new, :create]
+  # resource :users, only: [:new, :create]
   resources :users
 end
