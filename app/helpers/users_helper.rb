9ff1308e3e13bd1097bb6af4d0df277a0618030a
bigwizzo <wnnwillard@gmail.com>
2020-12-01 19:43:13 +0200

    Add helper methods for edit form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,16 @@
 module UsersHelper
+  attr_writer :clas1, :clas2, :clas3
   def user_params
     params.require(:user).permit(:user_name, :email, :password)
+  end
+
+  def edit_form_field_errors(msg)
+    if msg.include?('User name')
+      @clas1 = 'error'
+    elsif msg.include?('Email')
+      @clas2 = 'error'
+    elsif msg.include?('Password')
+      @clas3 = 'error'
+    end
   end
 end
